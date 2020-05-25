5444141b310699b1519c0b149270c05618e857e8
AndyO97 <47606055+AndyO97@users.noreply.github.com>
2020-05-24 23:53:15 -0400

    Update

--- public/js/event.js
+++ public/js/event.js
@@ -350,7 +350,7 @@ function getUserIdFetch(user){
 
             if(responseJSON){
                 let event = localStorage.getItem( 'EventId' );
-                console.log(responseJSON);
+                console.log(responseJSON[0]._id);
                 console.log("Adding participant");
                 addParticipantFetch(responseJSON[0]._id, event);
             }
