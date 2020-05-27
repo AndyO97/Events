fda5f16fc602637822f9d702ec8ad184f16eecec
AndyO97 <47606055+AndyO97@users.noreply.github.com>
2020-05-27 12:18:57 -0400

    Update

--- public/js/updateEvent.js
+++ public/js/updateEvent.js
@@ -155,7 +155,7 @@ function eventUpdateFetch( id, title, description, pictures, tags, date, dateIs,
                 results.innerHTML += `<div> Description: ${responseJSON.description} </div>`;
                 for(let j=0; j<responseJSON.pictures.length; j++){
                     //results.innerHTML += `<img src="${responseJSON.pictures[j]}" alt="Picture ${j+1} of event: ${responseJSON.title}"/>`;
-                    results.innerHTML += `<img src="${responseJSON.pictures[j]}" alt="Picture ${j+1} of event ${i+1}" width="500" height="333">`;
+                    results.innerHTML += `<img src="${responseJSON.pictures[j]}" alt="Picture ${j+1} of event ${responseJSON.title}" width="500" height="333">`;
                 }
                 results.innerHTML += `<div> Tags:`;
                 for(let k=0; k<responseJSON.tags.length; k++){
